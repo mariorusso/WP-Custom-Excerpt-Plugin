5f92c5d2e439a6fd66a04e82cf1b295f2cac4d5b
Mario Russo <mariorusso@gmail.com>
2015-03-11 15:37:52 -0500

    Initial commit, custom excerpt plugin

---
+++ custom_excerpt.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* Plugin Name: Custom Excerpt
+* Description: DIT 21063 1401 Assignment - Enables the user specify length of excerpt in words or characters. Allow user set a custom read more.   
+* Version: 1.0 
+* Author: Mario Russo
+* Author URI: http://www.russomario.com
+*/
+
