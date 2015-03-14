5486f91e6e28374e5126eb0378abaa1c8109a9a8
Mario Russo <mariorusso@gmail.com>
2015-03-13 22:39:45 -0500

    Everything working, need some detail in the code, and convert into a class. March 13 2015 10:39PM

--- custom_excerpt.php
+++ custom_excerpt.php
@@ -17,13 +17,117 @@ add_action('admin_menu', 'add_custom_excerpt_sections');
 
 add_action('admin_menu', 'add_custom_excerpt_fields');
 
-
-
+add_filter('the_excerpt', 'custom_excerpt');
 
 function custom_excerpt($excerpt) {
 	
+	$words = FALSE;
+	
+	$char = FALSE;
+	
+	$options = get_option('custom_excerpt_options');
+	
+	if(isset($options['length_insert'])){
+		if(empty($options['length_insert']) || $options['length_insert'] > 365){
+			$length = 365;
+		}else{
+			$length = $options['length_insert'];
+		}
+	}
+	
+	
+	if(isset($options['length_of_field'])){
+		if($options['length_of_field'] == 'words'){
+			$words = TRUE;
+		}
+		
+		if($options['length_of_field'] == 'chars'){
+			$char = TRUE;
+		}		
+	}
+	
+	if(isset($options['read_more_field'])){
+		if($options['read_more_field']){
+			$readmore = $options['read_more_field'];
+		}else{
+			$readmore = '[...]';
+		}
+		
+	}
+	
+	//if words selected, call the function to count the words.
+	if($words){
+		
+		return word_count($excerpt, $length, $readmore);		
+	}
+	
+	//if characters selected, call the function to count the characters.
+	if($char){
+		
+		return char_count($excerpt, $length, $readmore);		
+	}
+	
 }
-add_filter('the_excerpt', 'custom_excerpt');
+
+/*Function to count the characters. 
+ * accepts 2 parameters the excerpt and the length selected.
+ * replace the special characters from excerpt, 
+ * strip tags from the excerpt,
+ * cut the excerpt using the passed length,
+ * cut the excerpt on the last space so not cut words in the middle.
+ * Return the new excerpt between paragraph tags.
+ */
+function char_count($excerpt, $length, $readmore){
+		$newexcerpt = preg_replace(" (\[.*?\])", '', $excerpt);//replace the spacial characters form [&hellip;] in the end.
+		
+		$newexcerpt = strip_tags($newexcerpt);//Strip paragraph and other possible tags from the excerpt.  
+				
+		$newexcerpt = substr($newexcerpt, 0, $length+1);//Cut the excerpt using the passed length.
+		
+		$newexcerpt = substr($newexcerpt, 0, strripos($newexcerpt, " "));//Cut the excerpt on the last space so not cut words in the middle.
+		
+		//Set the permalink if checked and return the new excerpt with the permalink.
+		//Else return the without the permalink.		
+		$options = get_option('custom_excerpt_options');
+		if(isset($options['permalink_field']) && $options['permalink_field'] == 'yes'){
+			$permalink = get_permalink($post->ID);
+			$permalink = "<a href='$permalink'>";
+				return '<p>' . $newexcerpt . ' ' . $permalink . $readmore . '</a> </p>';
+		}else{
+			return '<p>' . $newexcerpt . ' ' . $readmore . '</p>';//Return the new excerpt.
+		}	 
+}
+
+/*Function to count the words. 
+ * accepts 2 parameters the excerpt and the length selected.
+ */
+function word_count($excerpt, $length, $readmore){
+	
+	$newexcerpt = strip_tags($excerpt);
+	
+	$newexcerpt = explode(' ', $newexcerpt, $length+1);
+	
+	array_pop($newexcerpt);
+	
+	$newexcerpt = implode(' ', $newexcerpt);
+	
+	//Set the permalink if checked and return the new excerpt with the permalink.
+	//Else return the without the permalink.
+	$options = get_option('custom_excerpt_options');
+		if(isset($options['permalink_field']) && $options['permalink_field'] == 'yes'){
+			$permalink = get_permalink($post->ID);
+			$permalink = "<a href='$permalink'>";
+				return '<p>' . $newexcerpt . ' ' . $permalink . $readmore . '</a> </p>';
+		}else{
+			return '<p>' . $newexcerpt . ' ' . $readmore . '</p>';//Return the new excerpt.
+		}	
+	
+	
+	
+	
+} 
+
+
 
 /* Register Plugin Settings
 ----------------------------------------------------------*/ 
@@ -90,7 +194,7 @@ function add_custom_excerpt_fields() {
 		
 	add_settings_field(
 		'length_insert', //id of the field
-		'Excerpt Length', //Title - Label of the field 
+		'Excerpt Length:', //Title - Label of the field 
 		'create_length_insert_field', //callback function to output the field.
 		'customexcerpt', //slug of the menu page
 		'general_options' //Section that the field should be in. 
@@ -103,21 +207,86 @@ function add_custom_excerpt_fields() {
 		'customexcerpt', //slug of the menu page
 		'general_options' //Section that the field should be in. 
 	);
+	
+	add_settings_field(
+		'read_more_field', //id of the field
+		'Read more text:', //Title - Label of the field 
+		'create_read_more_field', //callback function to output the field.
+		'customexcerpt', //slug of the menu page
+		'general_options' //Section that the field should be in. 
+	);
+	
+	add_settings_field(
+		'permalink_field', //id of the field
+		'Enable permalink?', //Title - Label of the field 
+		'create_permalink_checkbox', //callback function to output the field.
+		'customexcerpt', //slug of the menu page
+		'general_options' //Section that the field should be in. 
+	);
 }
 
 /*Output Length Insert Field
 ---------------------------------------------------------------*/
 function create_length_insert_field() {
+	
+	$options = get_option('custom_excerpt_options');
+		if(isset($options['length_insert'])){
+			$value = $options['length_insert'];
+		}else {
+			$value = '';
+		}
 				
-		echo "<input type='text' id='length_insert' name='custom_excerpt_options[length_insert]' />";
+		echo "<input type='text' id='length_insert' name='custom_excerpt_options[length_insert]' value='$value' />
+			  <br/><i>Defaults and max length 55(Words) and 365(Characters).</i>";
 }
 
 /*Output Length of: Field
 ---------------------------------------------------------------*/
 function create_length_of_field() {
+	
+	$options = get_option('custom_excerpt_options');
+	if(!isset($options['length_of_field'])){
+	  	$value = '';
+		$words_selected = 'selected';
+		$char_selected = '';
+	}else{
+		$value = esc_attr($options['length_of_field']);
+		$words_selected = ($value == 'words') ? 'selected' : '';
+		$char_selected = ($value == 'chars') ? 'selected' : '';
+	}
 				
 		echo "<select id='length_of_field' name='custom_excerpt_options[length_of_field]'>
-			  	<option value='characters' >Characters</option>
-			  	<option value='words'>Words</option>
+			  	<option value='chars' $char_selected>Characters</option>
+			  	<option value='words' $words_selected >Words</option>
 		      </select>";
+}
+
+/*Output Read More Field
+---------------------------------------------------------------*/
+function create_read_more_field() {
+	
+	$options = get_option('custom_excerpt_options');
+	
+		if(isset($options['read_more_field']) && !empty($options['read_more_field'])){
+			$value = $options['read_more_field'];
+		}else {
+			$value = '[...]';
+		}
+				
+		echo "<input type='text' id='read_more_field' name='custom_excerpt_options[read_more_field]' value='$value' />
+			  <br/><i>Defaults to [...].</i>";
+}
+
+/*Output Read More Field
+---------------------------------------------------------------*/
+function create_permalink_checkbox() {
+	$options = get_option('custom_excerpt_options');
+	if(isset($options['permalink_field'])){
+		$checked = 'checked';
+	}else{
+		$checked = '';
+	}	
+	
+	echo "<input type='checkbox' id='permalink_field' name=custom_excerpt_options[permalink_field]' value='yes' $checked />
+			<i>Add a link to the post in your readmore text.</i>";
 }
